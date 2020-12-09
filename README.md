407915c558d3f79e4f490cfa1cd0f781931c0504
cogitantium <falke.cs@gmail.com>
2020-12-09 14:13:40 +0100

    Add multi-run bench

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bench-solver
+
+Running on Mexican Standoff thrice:
+```
+Running: cd ../OnTheFlyATL/ && cargo run --package atl-checker --bin atl-checker -- solver --formula benches/lcgs/Mexican_Standoff/Mexican_Standoff_p1_is_alive_till_he_aint.json --model benches/lcgs/Mexican_Standoff/Mexican_Standoff.lcgs --model-type lcgs
+wall time: 0.20350737999251578, usertime: 0.904412, systime: 0.076476, peak 22824 KiB
+Running: cd ../OnTheFlyATL/ && cargo run --package atl-checker --bin atl-checker -- solver --formula benches/lcgs/Mexican_Standoff/Mexican_Standoff_p1_is_alive_till_he_aint.json --model benches/lcgs/Mexican_Standoff/Mexican_Standoff.lcgs --model-type lcgs
+wall time: 0.23878169899398927, usertime: 2.070686, systime: 0.126034, peak 23008 KiB
+Running: cd ../OnTheFlyATL/ && cargo run --package atl-checker --bin atl-checker -- solver --formula benches/lcgs/Mexican_Standoff/Mexican_Standoff_p1_is_alive_till_he_aint.json --model benches/lcgs/Mexican_Standoff/Mexican_Standoff.lcgs --model-type lcgs
+wall time: 0.24613207500078715, usertime: 3.637086, systime: 0.220474, peak 23008 KiB
+
+Results: Over 3 runs; avg walltime: 0.2295, avg usertime: 2.2041, avg systime: 0.1410, max peak 23008 KiB
+```
--- bench-solver.py
+++ bench-solver.py
@@ -1,11 +1,12 @@
 import argparse
+import statistics
 from resource import getrusage, RUSAGE_CHILDREN
 from timeit import timeit
 
 parser = argparse.ArgumentParser(description="Benchmark a tool.")
+parser.add_argument('num', type=int, nargs='?', help="Number of times to run command")
 parser.add_argument('command', type=str, nargs='*',
                     help="The command to benchmark, e.g. \"cd ../solver && solv -arg a -foo b -bar z\"")
-parser.add_argument('num', type=int, nargs='?', help="Number of times to run command")
 
 
 def bench(args):
@@ -22,4 +23,20 @@ def bench(args):
 if __name__ == '__main__':
     args = parser.parse_args()
     # args.command = "cargo run --package atl-checker --bin atl-checker -- solver --formula benches/lcgs/Mexican_Standoff/Mexican_Standoff_p1_is_alive_till_he_aint.json --model benches/lcgs/Mexican_Standoff/Mexican_Standoff.lcgs --model-type lcgs"
-    bench(args)
+
+    if args.num is not None and args.num > 1:
+        stats = {"wall_time": [], "ru_utime": [], "ru_stime": [], "ru_maxrss": []}
+        for n in range(0, args.num):
+            wall_time, ru_utime, ru_stime, ru_maxrss = bench(args)
+            stats["wall_time"].append(wall_time)
+            stats["ru_utime"].append(ru_utime)
+            stats["ru_stime"].append(ru_stime)
+            stats["ru_maxrss"].append(ru_maxrss)
+
+        stats["wall_time"] = statistics.mean(stats["wall_time"])
+        stats["ru_utime"] = statistics.mean(stats["ru_utime"])
+        stats["ru_stime"] = statistics.mean(stats["ru_stime"])
+        stats["ru_maxrss"] = max(stats["ru_maxrss"])
+        print(f"\nResults: Over {args.num} runs; avg walltime: {stats['wall_time']:.4f}, avg usertime: {stats['ru_utime']:.4f}, avg systime: {stats['ru_stime']:.4f}, max peak {stats['ru_maxrss']} KiB")
+    else:
+        bench(args)
