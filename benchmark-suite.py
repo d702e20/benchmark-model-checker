415604a788fd89ad840e01e2ac9217530c9c99e6
cogitantium <falke.cs@gmail.com>
2020-12-13 17:12:26 +0100

    Rework suite

--- benchmark-suite.py
+++ benchmark-suite.py
@@ -4,32 +4,42 @@ import os
 # Thread count combinations to use [1, 2, 4, ... , 32]
 THREADS = [pow(2, x) for x in range(0, 6)]
 
-ATL_SOLVER_PATH = "/some/path/atl-solver --option --"
-ATL_SOLVER_PROGRAMS = [{"program_path": "foo", "formula_path": "bar", "model-type": "lcgs"},
-                       {"program_path": "foo", "formula_path": "bar", "model-type": "lcgs"}]
+# Setup programs and examples to benchmark
+ATL_SOLVER_PATH = "cd ../OnTheFlyATL && cargo run --package atl-checker --bin atl-checker -- solver"
+ATL_SOLVER_PROGRAMS = [{"model": "foo", "formula": "bar"},
+                       {"model": "foo", "formula": "bar"}]
 
 PRISM_PATH = "/some/path/prism-games --option --"
-PRISM_PROGRAMS = [{"program_path": "foo", "formula_path": "bar", "model-type": "lcgs"},
-                  {"program_path": "foo", "formula_path": "bar", "model-type": "lcgs"}]
+PRISM_PROGRAMS_PATH = "some/path"
+PRISM_PROGRAMS = [{"model": "foo", "formula": "bar"},
+                  {"model": "foo", "formula": "bar"}]
 
+ATL_SOLVER_PROGRAMS_PATH = "docs/lcgs/working-examples/Mexican_Standoff_bigg"
+ATL_SOLVER_PROGRAMS = [
+    {"model": "Mexican_standoff_6.lcgs", "formula": "Mexican_Standoff_p1_is_alive_till_he_aint.json"},
+    {"model": "Mexican_standoff_7.lcgs", "formula": "Mexican_Standoff_p1_is_alive_till_he_aint.json"}]
 
 # make logdir
 os.makedirs("logs", exist_ok=True)
 
 # setup for atl_solver
 for proc in ATL_SOLVER_PROGRAMS:
-    for thread in THREADS:
-        print(f"Running benchmark for atl_solver, program: {proc}, thread count: {thread}")
-        with open(f"logs/{proc['program_path']}-{proc['formula_path']}-{thread}_threads.txt", "w+") as f:
+    for threads in THREADS:
+        print(f"Running benchmark for atl_solver, program: {proc}, thread count: {threads}")
+        with open(f"logs/{proc['model']}-{proc['formula']}-{threads}_threads.txt", "w+") as f:
+            # with open(f"logs/{proc['model']}-{proc['formula']}-{threads}_threads.txt", "w+") as f:
+            # Setup the program to bench, such that the command reads 'python(3) bench-solver.py "program"'
             subprocess.run(
-                f' --model path/to/{proc["program_path"]} --formula path/to/{proc["formula_path"]} --model-type {proc["model-type"]}',
+                f'python bench-solver.py \"{ATL_SOLVER_PATH} --model {ATL_SOLVER_PROGRAMS_PATH}/{proc["model"]} --formula '
+                f'{ATL_SOLVER_PROGRAMS_PATH}/{proc["formula"]} --model-type lcgs --threads {threads}\"',
                 stdout=f, shell=True)
 
+exit(0)  # FIXME: prism not yet setup
 # setup for prism
 for proc in PRISM_PROGRAMS:
-    for thread in THREADS:
-        print(f"Running benchmark for prism, program: {proc}, thread count: {thread}")
-        with open(f"logs/{proc['program_path']}-{proc['formula_path']}-{thread}_threads.txt", "w+") as f:
+    for threads in THREADS:
+        print(f"Running benchmark for prism, program: {proc}, thread count: {threads}")
+        with open(f"logs/{proc['model']}-{proc['formula']}-{threads}_threads.txt", "w+") as f:
             subprocess.run(
-                f'prism-binary --model path/to/{proc["program_path"]} --formula path/to/{proc["formula_path"]}',
+                f'prism-binary --model path/to/{proc["model"]} --formula path/to/{proc["formula"]}',
                 stdout=f, shell=True)
