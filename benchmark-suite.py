dcbe1e0f1cdd865a5538c07ec30f9998bf5f5bce
falkecarlsen <11318702+falkecarlsen@users.noreply.github.com>
2023-07-04 14:28:32 +0200

    abstract runner out of bench-solver and into main

--- benchmark-suite.py
+++ benchmark-suite.py
@@ -1,13 +1,20 @@
 import os
 import subprocess
+import time
 from datetime import datetime
+from resource import getrusage, RUSAGE_CHILDREN
+
 import pandas as pd
+import re
+from datetime import timedelta
 
 # VARIABLES
 PRISM = True
 SUITE = "prism-suite.csv"
 # choice of ["bfs", "dfs", "los", "lps", "dhs", "ihs", "lrs", "global"]
 SEARCH_STRATEGIES = ["bfs", "dfs", "los", "lps", "dhs", "ihs", "lrs", "global"] if not PRISM else ["none"]
+
+ENV = 'PATH="/scratch/user@domain.aau.dk/prism-games/jdk/jdk-20.0.1/bin:$PATH"'
 
 for SEARCH_STRATEGY in SEARCH_STRATEGIES:
     SUBCMD = SEARCH_STRATEGY if SEARCH_STRATEGY == "global" else "solver"
@@ -38,7 +45,12 @@ for SEARCH_STRATEGY in SEARCH_STRATEGIES:
     # subprocess.run(f"cd {CGAAL_DIR} && cargo build --release", stdout=subprocess.PIPE, shell=True)
 
     # setup dataframe to store results
-    df = pd.DataFrame(columns=['name', 'model', 'formula', 'threads', 'time_s', 'memory_MB', 'runs'])
+    if PRISM:
+        df = pd.DataFrame(
+            columns=['name', 'model', 'formula', 'time_s', 'memory_MB', 'total_reported', 'construction', 'checking'])
+    else:
+        df = pd.DataFrame(
+            columns=['name', 'model', 'formula', 'threads', 'time_s', 'memory_MB', 'search_strategy'])
 
     # read in suite from csv
     suite = pd.read_csv(SUITE, skipinitialspace=True, converters={'name': strip, 'model': strip, 'formula': strip})
@@ -67,11 +79,12 @@ for SEARCH_STRATEGY in SEARCH_STRATEGIES:
     for index, row in suite.iterrows():
         for threads in THREADS:
             print(
-                f"[{SEARCH_STRATEGY}{index}/{len(suite)}] {row['name']}/{threads} with model: '{row['model']}' and formula: '{row['formula']}'")
+                f"[{SEARCH_STRATEGY}|{index}/{len(suite)}] {row['name']}/{threads} with model: '{row['model']}' and formula: '{row['formula']}'")
             try:
                 if not PRISM:
+                    max_mem_before = getrusage(RUSAGE_CHILDREN).ru_maxrss / 1024
+                    start_time = time.perf_counter()
                     proc = subprocess.run(
-                        f'python3 bench-solver.py "'
                         f'{CGAAL_BIN} {SUBCMD} '
                         f'-f {CGAAL_EXAMPLES_DIR}{row["formula"]} '
                         f'-m {CGAAL_EXAMPLES_DIR}{row["model"]} '
@@ -79,20 +92,42 @@ for SEARCH_STRATEGY in SEARCH_STRATEGIES:
                         f'{SEARCH_STRATEGY_ARG} {"" if SEARCH_STRATEGY == "global" else SEARCH_STRATEGY} '
                         f'--quiet"',
                         shell=True, check=True, capture_output=True, text=True)
-                    out = [e.strip() for e in proc.stdout.split(',')]
+                    end_time = time.perf_counter()
+                    max_mem_after = getrusage(RUSAGE_CHILDREN).ru_maxrss / 1024
+
+                    # 'name', 'model', 'formula', 'threads', 'time_s', 'memory_MB', 'search_strategy'
+                    df.loc[len(df)] = [row['name'], row['model'], row['formula'], threads, end_time - start_time,
+                                       max_mem_after - max_mem_before, SEARCH_STRATEGY]
+
+
                 else:
+                    max_mem_before = getrusage(RUSAGE_CHILDREN).ru_maxrss / 1024
+                    start_time = time.perf_counter()
                     proc = subprocess.run(
-                        f'python3 bench-solver.py "'
+                        f'{ENV} '
                         f'{PRISM_BIN} '
                         f'{PRISM_EXAMPLES_DIR}{row["model"]} '
                         f'{PRISM_EXAMPLES_DIR}{row["formula"]} ',
                         shell=True, check=True, capture_output=True, text=True)
-                    out = [e.strip() for e in proc.stdout.split(',')]
+                    end_time = time.perf_counter()
+                    max_mem_after = getrusage(RUSAGE_CHILDREN).ru_maxrss / 1024
+                    # extract time for model construction and model checking
+                    prism_times = [(line.split()[3], timedelta(seconds=float(re.findall(r'[\d\.\d]+', line)[0]))) for
+                                   line in
+                                   proc.stdout.splitlines() if
+                                   line.startswith("Time for model")]
+                    # add total time
+                    prism_times += [('total reported', sum([t[1] for t in prism_times], timedelta(seconds=0)))]
 
-                # add row to df
-                df.loc[len(df)] = [row['name'], row['model'], row['formula'], threads, out[0], out[1], out[2]]
+                    # 'name', 'model', 'formula', 'time_s', 'memory_MB', 'total_reported', construction', 'checking'
+                    # add row to df
+                    df.loc[len(df)] = [row['name'], row['model'], row['formula'],
+                                       end_time - start_time,
+                                       max_mem_before - max_mem_after,
+                                       prism_times[2][1].total_seconds(),
+                                       prism_times[0][1].total_seconds(),
+                                       prism_times[1][1].total_seconds()]
 
-                print(proc.stdout.strip())
             except subprocess.CalledProcessError as e:
                 print(f"Failed to start bench. Error: {e}")
                 continue
